4da73dc62d7c0a87d8a88f6319482bfd9838533c
jeantorre <jean.torre21@gmail.com>
2024-07-14 18:25:54 -0300

    resolução exercício 04

--- exercicios_04.py
+++ exercicios_04.py
@@ -18,19 +18,36 @@
 '''
 3 - Crie um dicionário para armazenar informações de um livro, incluindo título, autor e ano de publicação. Imprima cada informação.
 '''
-dicionario_livro: dict = {
-    'Autor': 'Napoleon Hill',
-    'Título': 'Mais Espero que o Diabo',
-    'Ano': 2020
-}
-# print(dicionario_livro)
+# dicionario_livro: dict = {
+#     'Autor': 'Napoleon Hill',
+#     'Título': 'Mais Espero que o Diabo',
+#     'Ano': 2020
+# }
+# # print(dicionario_livro)
 
-for item in dicionario_livro.items():
-    print(item)
+# for item in dicionario_livro.items():
+#     print(item)
 
 '''
 4 - Escreva um programa que conta o número de ocorrências de cada caractere em uma string usando um dicionário.
 '''
+def contador_caracteres(frase: str):
+    contagem = {}
+    for caracter in frase:
+        if caracter not in contagem:
+            contagem[caracter] = 1
+        else:
+            contagem[caracter] += 1
+    return contagem
+
+frase: str = input('Digite uma frase: ')
+frase = frase.lower()
+# print(contador_caracteres(frase))
+
+contagem_ordenada: dict = dict(sorted(contador_caracteres(frase).items()))
+for item in contagem_ordenada.items():
+    print(item)
+
 
 '''
 5 - Dada a lista ["maçã", "banana", "cereja"] e o dicionário {"maçã": 0.45, "banana": 0.30, "cereja": 0.65}, calcule o preço total da lista de compras.
