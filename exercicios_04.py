eb8002985b813d82f424563d87a590097d7d254e
jeantorre <jean.torre21@gmail.com>
2024-07-14 18:07:09 -0300

    resolução exercício 3

--- exercicios_04.py
+++ exercicios_04.py
@@ -9,15 +9,24 @@
 '''
 2 - Dada a lista ["Python", "Java", "C++", "JavaScript"], remova o item "C++" e adicione "Ruby".
 '''
-lista_linguagem: list = ["Python", "Java", "C++", "JavaScript"]
-# lista_linguagem.remove('C++') 
-lista_linguagem.pop(2)
-lista_linguagem.append('Ruby')
-print(lista_linguagem)
+# lista_linguagem: list = ["Python", "Java", "C++", "JavaScript"]
+# # lista_linguagem.remove('C++') 
+# lista_linguagem.pop(2)
+# lista_linguagem.append('Ruby')
+# print(lista_linguagem)
 
 '''
 3 - Crie um dicionário para armazenar informações de um livro, incluindo título, autor e ano de publicação. Imprima cada informação.
 '''
+dicionario_livro: dict = {
+    'Autor': 'Napoleon Hill',
+    'Título': 'Mais Espero que o Diabo',
+    'Ano': 2020
+}
+# print(dicionario_livro)
+
+for item in dicionario_livro.items():
+    print(item)
 
 '''
 4 - Escreva um programa que conta o número de ocorrências de cada caractere em uma string usando um dicionário.
